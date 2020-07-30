9b3fcb6aa58b361ddcd59045514cc91c6a3d3b70
snalama1 <snalama1@ford.com>
2020-07-30 15:18:47 +0000

    Added JDBC template classes.

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args){
         SpringApplication.run(PalTrackerApplication.class, args);
     }
     @Bean
-    TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        //return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
+        //return new
     }
 }
